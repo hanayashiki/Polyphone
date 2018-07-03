79f391d9aed5c168864f025387d93178d6eaa250
hanyashiki <494450105@qq.com>
2018-07-03 16:53:02 +0800

    New words

--- scratches/open_word_vec.py
+++ scratches/open_word_vec.py
@@ -9,7 +9,7 @@ i = 0
 for line in f:
     #print(line.split(' '))
     l = line.strip().split(' ')
-    if re.match(r'^[a-zA-Z，。？（）\u4e00-\u9fbb]$', l[0]):
+    if re.match(r'^[a-zA-Z，。？：；─…《 》（）“”“”\u4e00-\u9fbb]$', l[0]):
         g.write(','.join(l) + '\n')
         i += 1
 
